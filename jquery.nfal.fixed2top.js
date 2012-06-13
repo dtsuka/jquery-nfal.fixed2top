fb560d3041f46c5067b12f4a5f59e285ea8278a9
Daisuke Tsukada <dtsuka@gmail.com>
2012-06-13 20:25:14 +0900

    全体的に大幅修正
    ・スクロールへの追従をaddClass()ではなくcss()で実装
    ・iOS等position:fixedに対応していなかったり不具合がある場合の対応として、position:absoluteで代替する処理を追加
    （userAgentで判断する）
    ・バージョン番号を1桁デノミ

--- jquery.nfal.fixed2top.js
+++ jquery.nfal.fixed2top.js
@@ -1,42 +1,61 @@
  /*!
- * NFAL Fixed to top  - jQuery Plugin
+ * NFAL Fixed to Top  - jQuery Plugin
  *
  * Copyright 2012, Near Future Arts Lab Co.,Ltd.
  * Released under the MIT License.
  *
- * Version: 1.1.1 (13/06/2012)
+ * Version: 0.2.1 (13/06/2012)
  */
 (function ($) {
 	$.fn.fixed2top = function(options) {
 		var opts = $.extend({}, $.fn.fixed2top.defaults, options);
+
+		if (!window.console){
+			window.console = {
+				log: function(){}
+			};
+		}
 		var debug = function (str) {
-            if (opts.debugmode) console.log(str);
+            if (opts.debugMode) console.log(str);
 		};
 
-		var $window			= $(window),
-			$document		= $(document);
+		var $window		= $(window),
+			$document	= $(document);
+
+		var positionfixed = true,
+			ua = navigator.userAgent,
+			ios = /iphone|ipod|ipad/i.test(ua),
+			android = /android/i.test(ua),
+			ie6 = /MSIE 6/i.test(ua);
+		if(ios || android || ie6){
+			positionfixed = false;
+		}
+
 
 		return this.each(function () {
 			var $this			= $(this),
 				fixedClass		= opts.fixedClass,
 				absoluteClass	= opts.absoluteClass,
 				offsetTop		= opts.offsetTop,
-				paddingBtm		= opts.paddingBottom,
+				$footer			= $(opts.footerTarget),
+				footerOffsetTop	= $footer.offset().top,
+				footerHeight	= $footer.outerHeight(true),
+				marginBtm		= opts.marginBottom,
 				liquid			= opts.liquidLayout,
 				defaBottom		= $this.css('bottom'),
 				defaWidth		= $this.width(),
 				pare			= $this.parent(),
 				pareWidth		= $this.parent().width();
 
-			
 
-			footerHeightGet = function () {
-				paddingBtm = $(opts.footerTarget).outerHeight(true) + opts.paddingBottom;
+			var footerHeightGet = function () {
+				footerOffsetTop	= $footer.offset().top;
+				footerHeight	= $footer.outerHeight(true);
+				marginBtm = footerHeight + opts.marginBottom;
 			};
-			pareWidthGet = function(){
+			var pareWidthGet = function(){
 				pareWidth = $this.parent().width();
 			};
-
 
 
 			if(liquid){
@@ -51,13 +70,13 @@
 			if(offsetTop == 'auto'){
 				offsetTop = $this.offset().top;
 			}
-			debug('paddingBtm:'+ paddingBtm);
+			debug('marginBtm:'+ marginBtm);
 			debug('offsetTop:'+ offsetTop);
 
-			function adjust(){
+			var adjust = function(){
 				if($window.scrollTop() < offsetTop){
 					debug('#A static');
-					//$this.removeClass(fixedClass).removeClass(absoluteClass);
+					$this.removeClass(fixedClass).removeClass(absoluteClass);
 					$this.css({
 						position:'static',
 						top:'auto',
@@ -68,45 +87,46 @@
 							width:pareWidth+'px'
 						});
 					}
-				}else if($document.height() - $window.scrollTop() -paddingBtm < $this.outerHeight(true)){
+				}else if($document.height() - $window.scrollTop() -marginBtm < $this.outerHeight(true)){
 					debug('#B absolute(bottom end)');
-					//$this.removeClass(fixedClass).addClass(absoluteClass);
+					$this.removeClass(fixedClass).addClass(absoluteClass);
 					$this.css({
 						position:'absolute',
-						top:$(opts.footerTarget).offset().top - $this.outerHeight(true) + ($(opts.footerTarget).outerHeight() - paddingBtm)+'px',
+						top:footerOffsetTop - $this.outerHeight(true) + footerHeight - marginBtm+'px',
 						width:pareWidth+'px'
 					});
-					debug($(opts.footerTarget).offset().top);
 				}else {
 					debug('#C fixed');
-					//$this.addClass(fixedClass).removeClass(absoluteClass);
-					$this.css({
-						position:'absolute',
-						left:0,
-						top:$window.scrollTop() + pare.offset().top+'px',
-						width:pareWidth+'px'
-					});
-					/*$this.css({
-						position:'fixed',
-						top:0,
-						bottom:defaBottom,
-						width:pareWidth+'px'
-					});*/
-
+					$this.addClass(fixedClass).removeClass(absoluteClass);
+					if(positionfixed){
+						$this.css({
+							position:'fixed',
+							top:0,
+							bottom:defaBottom,
+							width:pareWidth+'px'
+						});
+					}else{
+						$this.css({
+							position:'absolute',
+							left:0,
+							top:$window.scrollTop() + pare.offset().top+'px',
+							width:pareWidth+'px'
+						});
+					}
 				}				
-			}
+			};
 			adjust();
 			$window.bind('scroll resize', adjust);
 		});
 		
 	};
 	$.fn.fixed2top.defaults = {
-		debugmode		: false,	//デバッグモード
+		debugMode		: false,	//デバッグモード
 		offsetTop		: 'auto',	//基本的にautoのままで使用
 		footerTarget	: null,		//フッターに被せないようにする場合は、ここにフッターを指定
-		paddingBottom	: 0,		//フッターに被せないようにする場合は、ここにフッターの高さを数値指定
+		marginBottom	: 0,		//対象エレメントの下に空けるマージン値
+		liquidLayout	: false,	//リキッドレイアウトの場合はtrue
 		fixedClass		: 'f2t_scrolling',	//スクロール中に追加されるクラス名
-		liquidLayout	: false,	//リキッドレイアウトの場合true
 		absoluteClass	: 'f2t_bottomEnd'	//下端までスクロールされた時に追加されるクラス名
 	};
 
